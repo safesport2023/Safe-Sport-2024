dd8f362b5eecf2df98cd4c176858596289c09f0e
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2024-01-31 12:57:54 -0500

    readded new authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "safesport20245b6bff87": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}

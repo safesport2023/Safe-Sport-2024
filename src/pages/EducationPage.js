63406652c05245dedee46c3e629dfabf6c73e0d4
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2024-02-29 16:47:51 -0500

    added bilingual on home and education and will finish this today

--- src/pages/EducationPage.js
+++ src/pages/EducationPage.js
@@ -17,16 +17,30 @@ import { InView } from 'react-intersection-observer';
 Amplify.configure(awsconfig);
 
 const EducationPage = () => {
-    const [videoStatus, setVideoStatus] = useState([
-        { unlocked: true, watched: false },
-        { unlocked: false, watched: false },
-        { unlocked: false, watched: false },
-        { unlocked: false, watched: false },
-        { unlocked: false, watched: false },
-        { unlocked: false, watched: false },
-        { unlocked: false, watched: false },
-        { unlocked: false, watched: false },
-    ]);
+    const [videoStatus, setVideoStatus] = useState(() => {
+        // Initialize videoStatus from localStorage if available, or use the default state
+        const storedStatus = localStorage.getItem('videoStatus');
+        return storedStatus ? JSON.parse(storedStatus) : [
+            { unlocked: true, watched: false },
+            { unlocked: false, watched: false },
+            { unlocked: false, watched: false },
+            { unlocked: false, watched: false },
+            { unlocked: false, watched: false },
+            { unlocked: false, watched: false },
+            { unlocked: false, watched: false },
+            { unlocked: false, watched: false },
+
+        ]
+    });
+
+    useEffect(() => {
+        // Update localStorage whenever videoStatus changes
+        localStorage.setItem('videoStatus', JSON.stringify(videoStatus));
+        const allVideosWatched = videoStatus.every(video => video.watched);
+        if (allVideosWatched) {
+            console.log('User has watched all the videos!');
+        }
+    }, [videoStatus]);
 
     //language handler 
     const [language, setLanguage] = useState('english'); // Default language is English
@@ -96,7 +110,9 @@ const EducationPage = () => {
         <InView>
             <div className="education-page">
                 <div className='page-title-div'>
-                    <h1 className='page-title'>Educational Video Content</h1>
+                    <h1 className='page-title'>{language === 'english' ? 'Educational Video Content' : 'Contenu Vidéo Éducatif'}</h1>
+
+
                 </div>
                 <Authenticator className='education-page-authenticator'>
                     {({ signOut, user }) => (
@@ -130,8 +146,10 @@ const EducationPage = () => {
                                     <source src={language === 'english' ? videoUrls.intro : videoUrls.intro} type="video/mp4" />
                                 </video>
                                 <div className='titles-container'>
-                                    <h3 className="video-title">Introduction</h3>
-                                    <p className='video-info'>Safe Sport for Youth Intro - We will explain how these educational videos will flow, what we hope and trust you will learn from them, and set you up for success in sport.</p>
+                                    <h3 className="video-title">{language === 'english' ? 'Introduction' : 'Introduction'}</h3>
+
+                                    <p className='video-info'>{language === 'english' ? `Safe Sport for Youth Intro - We will explain how these educational videos will flow, what we hope and trust you will learn from them, and set you up for success in sport.` : `Intro pour Safe Sport pour les Jeunes - Nous expliquerons comment ces vidéos éducatives seront organisées, ce que nous espérons et croyons que vous apprendrez d'elles, et vous préparerons pour réussir dans le sport.`}</p>
+
                                 </div>
 
 
@@ -152,8 +170,10 @@ const EducationPage = () => {
                                     {videoStatus[1].unlocked ? null : <div className="lock-overlay"></div>}
                                 </div>
                                 <div className='titles-container'>
-                                    <h3 className="video-title">Bullying</h3>
-                                    <p className='video-info'>We've all heard of bullying, but it may show up differently in sport than at school or other  other settings. Let's look at how bullying can be a reality in sport today and equip ourselves with the knowledge and support available to stop bullying in its tracks. </p>
+                                    <h3 className="video-title">{language === 'english' ? 'Bullying' : 'Intimidation'}</h3>
+
+                                    <p className='video-info'>{language === 'english' ? "We've all heard of bullying, but it may show up differently in sport than at school or other settings. Let's look at how bullying can be a reality in sport today and equip ourselves with the knowledge and support available to stop bullying in its tracks." : "Nous avons tous entendu parler de l'intimidation, mais elle peut se manifester différemment dans le sport qu'à l'école ou dans d'autres contextes. Voyons comment l'intimidation peut être une réalité dans le sport aujourd'hui et équipons-nous des connaissances et du soutien disponibles pour arrêter l'intimidation sur place."}</p>
+
                                 </div>
 
 
@@ -171,8 +191,10 @@ const EducationPage = () => {
                                     {videoStatus[2].unlocked ? null : <div className="lock-overlay"></div>}
                                 </div>
                                 <div className='titles-container'>
-                                    <h3 className="video-title">Hazing</h3>
-                                    <p className='video-info'>Often masked as tradition, hazing is sometimes hard to identify. Just because it's happened in the past, doesn't make it ok today. This video helps us to identify what  hazing is, and if it is happening to you. We provide solutions to ending any tolerance for hazing. It's not ok.</p>
+                                    <h3 className="video-title">{language === 'english' ? 'Hazing' : 'Bizutage'}</h3>
+
+                                    <p className='video-info'>{language === 'english' ? "Often masked as tradition, hazing is sometimes hard to identify. Just because it's happened in the past, doesn't make it ok today. This video helps us to identify what hazing is, and if it is happening to you. We provide solutions to ending any tolerance for hazing. It's not ok." : "Souvent masqué comme une tradition, le bizutage est parfois difficile à identifier. Le fait que cela se soit produit dans le passé ne le rend pas acceptable aujourd'hui. Cette vidéo nous aide à identifier ce qu'est le bizutage et s'il se produit à votre égard. Nous proposons des solutions pour mettre fin à toute tolérance envers le bizutage. Ce n'est pas acceptable."}</p>
+
                                 </div>
 
 
@@ -191,8 +213,10 @@ const EducationPage = () => {
                                     {videoStatus[3].unlocked ? null : <div className="lock-overlay"></div>}
                                 </div>
                                 <div className='titles-container'>
-                                    <h3 className="video-title">Boundary Transgressions</h3>
-                                    <p className='video-info'>Coaches and any other persons of authority must maintain professional boundaries with athletes. The video explains why this is important and how to identify red flags or warning signs that your boundaries are being crossed.</p>
+                                    <h3 className="video-title">{language === 'english' ? 'Boundary Transgressions' : 'Transgressions des limites'}</h3>
+
+                                    <p className='video-info'>{language === 'english' ? 'Coaches and any other persons of authority must maintain professional boundaries with athletes. The video explains why this is important and how to identify red flags or warning signs that your boundaries are being crossed.' : 'Les entraîneurs et toute autre personne ayant une autorité doivent maintenir des limites professionnelles avec les athlètes. La vidéo explique pourquoi c`est important et comment identifier les signaux d`alarme ou les signes avant-coureurs indiquant que vos limites sont franchies.'}</p>
+
                                 </div>
 
 
@@ -212,8 +236,8 @@ const EducationPage = () => {
                                     {videoStatus[4].unlocked ? null : <div className="lock-overlay"></div>}
                                 </div>
                                 <div className='titles-container'>
-                                    <h3 className="video-title">Grooming</h3>
-                                    <p className='video-info'>Grooming may be a new term for you. The video demonstrates common steps that can be used to groom youth and athletes so you can identify them in real life. We provide first hand examples from survivors of grooming and explain how a groomer can try to manipulate athletes into going along with this form of abuse</p>
+                                    <h3 className="video-title">{language === 'english' ? 'Grooming' : 'Toilettage'}</h3>
+                                    <p className='video-info'>{language === 'english' ? 'Grooming may be a new term for you. The video demonstrates common steps that can be used to groom youth and athletes so you can identify them in real life. We provide first hand examples from survivors of grooming and explain how a groomer can try to manipulate athletes into going along with this form of abuse' : 'Le toilettage peut être un nouveau terme pour vous. La vidéo montre les étapes communes qui peuvent être utilisées pour toiletter les jeunes et les athlètes afin que vous puissiez les identifier dans la vraie vie. Nous fournissons des exemples concrets de survivants du toilettage et expliquons comment un toiletteur peut essayer de manipuler les athlètes pour qu`ils acceptent cette forme d`abus.'}</p>
                                 </div>
 
 
@@ -232,8 +256,9 @@ const EducationPage = () => {
                                     {videoStatus[5].unlocked ? null : <div className="lock-overlay"></div>}
                                 </div>
                                 <div className='titles-container'>
-                                    <h3 className="video-title">Discrimination, Psychological, and Physical Maltreatment</h3>
-                                    <p className='video-info'>Hang in there, you are doing great. In the next two videos we will cover the other forms of maltreatment we feel are important so keep those eyes and ears open, you’re almost there. </p>
+                                    <h3 className="video-title">{language === 'english' ? 'Discrimination, Psychological, and Physical Maltreatment' : 'Discrimination, Maltraitance Psychologique et Physique'}</h3>
+
+                                    <p className='video-info'>{language === 'english' ? 'Hang in there, you are doing great. In the next two videos we will cover the other forms of maltreatment we feel are important so keep those eyes and ears open, you’re almost there.' : 'Accrochez-vous, vous vous en sortez très bien. Dans les deux prochaines vidéos, nous aborderons les autres formes de maltraitance que nous estimons importantes, alors gardez les yeux et les oreilles ouverts, vous y êtes presque.'}</p>
                                 </div>
 
 
@@ -252,8 +277,9 @@ const EducationPage = () => {
                                     {videoStatus[6].unlocked ? null : <div className="lock-overlay"></div>}
                                 </div>
                                 <div className='titles-container'>
-                                    <h3 className="video-title">Neglect, Sexual Maltreatment, and Micro-aggressions</h3>
-                                    <p className='video-info'>Maltreatment often starts with subtle micro-aggressions, which can be harder to identify. Let's dig in to what those are along with two more forms of maltreatment some athletes may encounter.</p>
+                                    <h3 className="video-title">{language === 'english' ? 'Neglect, Sexual Maltreatment, and Micro-aggressions' : 'Négligence, Maltraitance Sexuelle et Micro-agressions'}</h3>
+                                    <p className='video-info'>{language === 'english' ? 'Maltreatment often starts with subtle micro-aggressions, which can be harder to identify. Let`s dig in to what those are along with two more forms of maltreatment some athletes may encounter.' : 'La maltraitance commence souvent par des micro-agressions subtiles, qui peuvent être plus difficiles à identifier. Explorons ce que sont ces micro-agressions ainsi que deux autres formes de maltraitance que certains athlètes peuvent rencontrer.'}</p>
+
                                 </div>
 
 
@@ -271,8 +297,9 @@ const EducationPage = () => {
                                     {videoStatus[7].unlocked ? null : <div className="lock-overlay"></div>}
                                 </div>
                                 <div className='titles-container'>
-                                    <h3 className="video-title">Report and Support</h3>
-                                    <p className='video-info'>Final video! And an important one as we will discuss how you can report maltreatment and get support. Recongizing maltreatment is key so that you can speak out and get the help and support you need. We hope you enjoyed these short informative videos and we wish you all the best on your athletic journey, wherever that takes you. Stay safe and speak out.</p>
+                                    <h3 className="video-title">{language === 'english' ? 'Report and Support' : 'Signaler et Soutien'}</h3>
+                                    <p className='video-info'>{language === 'english' ? 'Final video! And an important one as we will discuss how you can report maltreatment and get support. Recognizing maltreatment is key so that you can speak out and get the help and support you need. We hope you enjoyed these short informative videos and we wish you all the best on your athletic journey, wherever that takes you. Stay safe and speak out.' : 'Dernière vidéo ! Et une vidéo importante car nous discuterons de la manière dont vous pouvez signaler les mauvais traitements et obtenir du soutien. Reconnaître les mauvais traitements est essentiel pour que vous puissiez vous exprimer et obtenir l`aide et le soutien dont vous avez besoin. Nous espérons que vous avez apprécié ces courtes vidéos informatives et nous vous souhaitons tout le meilleur dans votre parcours sportif, où que cela vous mène. Restez en sécurité et parlez.'}</p>
+
                                 </div>
 
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -85,7 +85,8 @@ const Home = () => {
     return (
         <div className='home-page'>
             <div className='page-title-div'>
-                <h1 className='page-title'>Welcome to Safe Sport <br></br> Education for Youth</h1>
+
+                <h1 className='page-title'>{language === 'english' ? 'Welcome to Safe Sport' : 'Bienvenue chez Safe Sport'} <br></br> {language === 'english' ? 'Education for Youth' : 'Éducation pour les Jeunes'}</h1>
             </div>
 
             <Authenticator className='home-page-authenticator' hideDefault={true}>
@@ -99,11 +100,11 @@ const Home = () => {
             <div className='lang-button-div'>
                 <button className='mobile-btn-lang' onClick={toggleLanguage}>Switch to {language === 'english' ? 'French' : 'English'}</button>
             </div>
-            {userEmail ? (
+            {/* {userEmail ? (
                 <div className="user-email-container">
                     <p>Your email: {userEmail}</p>
                 </div>
-            ) : null}
+            ) : null} */}
             <div className="video-Container" >
                 <video
                     id='HFYHVideo'
@@ -118,35 +119,38 @@ const Home = () => {
 
             </div>
             <div className='welcome-statement default-div'>
-                <p>Welcome to Safe Sport Education for Youth! This educational program will provide you with all the tools you need to have a safe and positive sports experience. Safe Sport Education for Youth is brought to you by the National Sports Organizations representing athletics, volleyball, and swimming. These videos are short and all you need to do is watch them. Easy right? Check out the videos and be a part of our new movement called Safe Sport.
-                    <br></br>
-                    <br></br>
+                <p>{language === 'english' ? `Welcome to Safe Sport Education for Youth! This educational program will provide you with all the tools you need to have a safe and positive sports experience. Safe Sport Education for Youth is brought to you by the National Sports Organizations representing athletics, volleyball, and swimming. These videos are short and all you need to do is watch them. Easy right? Check out the videos and be a part of our new movement called Safe Sport.` : `Bienvenue à l'éducation Safe Sport pour la jeunesse ! Ce programme éducatif vous fournira tous les outils nécessaires pour vivre une expérience sportive sûre et positive. L'éducation Safe Sport pour la jeunesse vous est proposée par les organisations nationales de sports représentant l'athlétisme, le volleyball et la natation. Ces vidéos sont courtes et tout ce que vous avez à faire est de les regarder. Facile non ? Regardez les vidéos et faites partie de notre nouveau mouvement appelé Safe Sport.`}</p>
 
-                    Every athlete deserves a sports experience that's free of maltreatment and abuse. Sports can provide the fun and positive environment you deserve, but sometimes it doesn't exactly work out that way.We have tailored this education program to provide you with the tools to protect yourself from negative sporting situations. This program has been designed specifically for athletes age 13-19. If you are under 13, please get permission from your parent(s) or guardian(s) before you start the program.
+                <br></br>
+                <br></br>
+                <p>{language === 'english' ? `Every athlete deserves a sports experience that's free of maltreatment and abuse. Sports can provide the fun and positive environment you deserve, but sometimes it doesn't exactly work out that way. We have tailored this education program to provide you with the tools to protect yourself from negative sporting situations. This program has been designed specifically for athletes age 13-19. If you are under 13, please get permission from your parent(s) or guardian(s) before you start the program.` : `Chaque athlète mérite une expérience sportive sans mauvais traitements ni abus. Le sport peut offrir l'environnement amusant et positif que vous méritez, mais parfois cela ne se passe pas exactement ainsi. Nous avons adapté ce programme éducatif pour vous fournir les outils nécessaires pour vous protéger des situations sportives négatives. Ce programme a été spécialement conçu pour les athlètes âgés de 13 à 19 ans. Si vous avez moins de 13 ans, veuillez obtenir la permission de vos parents ou tuteurs avant de commencer le programme.`}</p>
 
-                    <br></br>
-                    <br></br>
-                    Some of the information you hear may be hard to hear and could make you uncomfortable. We've got a page <Link className='custom-links' to="/resources"> (Resources)</Link> with links to some of the support and resources available to help you get through it. There are there to help you.  </p>
+                <br></br>
+                <br></br>
+                <p>{language === 'english' ? `Some of the information you hear may be hard to hear and could make you uncomfortable. We've got a page ` : `Certaines des informations que vous entendrez peuvent être difficiles à entendre et peuvent vous mettre mal à l'aise. Nous avons une page `}<Link className='custom-links' to="/resources"> (Resources)</Link>{language === 'english' ? ` with links to some of the support and resources available to help you get through it. There are there to help you.` : ` avec des liens vers certains des soutiens et ressources disponibles pour vous aider à y faire face. Ils sont là pour vous aider.`}</p>
+
             </div>
             <div className='gap-container'>
             </div>
             <div className='welcome-statement default-div'>
-                <p>So how does the Safe Sport for Youth program work. We kept it simple for anyone to use. Just follow these step by step instructions and be open to learning:
-                    <br></br>
-                    <br></br>
+                <p>{language === 'english' ? `So how does the Safe Sport for Youth program work. We kept it simple for anyone to use. Just follow these step by step instructions and be open to learning:` : `Alors, comment fonctionne le programme Safe Sport for Youth. Nous l'avons rendu simple pour que tout le monde puisse l'utiliser. Suivez simplement ces instructions étape par étape et soyez ouvert à l'apprentissage:`}</p>
 
+                <br></br>
+                <br></br>
+                <p>
                     <ul>
-                        <li>Register for the website using an email that we can send your certificate of completion to.</li>
-                        <br></br>
-                        <li>Watch the videos on the education page in order. No skipping ahead.</li>
-                        <br></br>
-                        <li>Stay present and pay attention to the videos. The information matters!</li>
-                        <br></br>
-                        <li>Recieve your certificate of completion by email and submit it to your sports club if they have asked for it.</li>
-                        <br></br>
-                        <li>Spread the word! We all need to do our part to keep sport safe.</li>
+                        <li>{language === 'english' ? 'Register for the website using an email that we can send your certificate of completion to.' : 'Inscrivez-vous sur le site Web en utilisant une adresse e-mail à laquelle nous pouvons envoyer votre certificat de réussite.'}</li>
+                        <br />
+                        <li>{language === 'english' ? 'Watch the videos on the education page in order. No skipping ahead.' : 'Regardez les vidéos sur la page éducative dans l`ordre. Ne pas sauter.'}</li>
+                        <br />
+                        <li>{language === 'english' ? 'Stay present and pay attention to the videos. The information matters!' : 'Restez présent et faites attention aux vidéos. Les informations sont importantes!'}</li>
+                        <br />
+                        <li>{language === 'english' ? 'Receive your certificate of completion by email and submit it to your sports club if they have asked for it.' : 'Recevez votre certificat de réussite par e-mail et soumettez-le à votre club de sport s`ils vous l`ont demandé.'}</li>
+                        <br />
+                        <li>{language === 'english' ? 'Spread the word! We all need to do our part to keep sport safe.' : 'Passez le mot! Nous devons tous faire notre part pour garder le sport sûr.'}</li>
                     </ul>
                 </p>
+
             </div>
 
 
